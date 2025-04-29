e0d691cb268985d23448fa9c341c538fbc88ac17
Makion65 <52094403+Makion65@users.noreply.github.com>
2025-04-29 16:06:50 -0500

    Initial commit

---
+++ src/main/java/net/makion/customgem/CustomGemClient.java
@@ -0,0 +1,10 @@
+package net.makion.customgem;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CustomGemClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/makion/customgem/CustomGemDataGenerator.java
@@ -0,0 +1,11 @@
+package net.makion.customgem;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CustomGemDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
